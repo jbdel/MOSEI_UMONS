f80a1ba5626a235eceea3a99758b6511cba8c127
jbdel <jeanbenoit.delbrouck@gmail.com>
2020-04-08 17:13:57 +0200

    Add files via upload

--- model_bi.py
+++ model_bi.py
@@ -1,26 +1,52 @@
-import torch.nn as nn
-
-from layers.fc import MLP
-from layers.layer_norm import LayerNorm
-
+import math
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from net import SA, SGA
-import torch
-import math
-from layers.fc import FC
 
-# Masking the sequence mask
+from layers.fc import MLP, FC
+from layers.layer_norm import LayerNorm
+
+# ------------------------------------
+# ---------- Masking sequence --------
+# ------------------------------------
 def make_mask(feature):
     return (torch.sum(
         torch.abs(feature),
         dim=-1
     ) == 0).unsqueeze(1).unsqueeze(2)
 
+# ----------------------------------------------
+# ---------- Attention at end of block --------
+# ----------------------------------------------
+
+class Att_solo(nn.Module):
+    def __init__(self, args, FLAT_GLIMPSES):
+        super(Att_solo, self).__init__()
+        self.args = args
+        self.linear = nn.Linear(args.ff_size, FLAT_GLIMPSES)
+        self.FLAT_GLIMPSES = FLAT_GLIMPSES
+
+    def forward(self, x, q, x_mask=None):
+        att = self.linear(q)
+        if x_mask is not None:
+            att = att.masked_fill(
+                x_mask.squeeze(1).squeeze(1).unsqueeze(2),
+                -1e9
+            )
+        att = F.softmax(att, dim=1)
+
+        att_list = []
+        for i in range(self.FLAT_GLIMPSES):
+            att_list.append(
+                torch.sum(att[:, :, i: i + 1] * x, dim=1)
+            )
+
+        return torch.stack(att_list).transpose_(0, 1)
 
 # ------------------------------
 # ---------- Flattening --------
 # ------------------------------
+
 
 class AttFlat(nn.Module):
     def __init__(self, args):
@@ -56,6 +82,71 @@ class AttFlat(nn.Module):
         return x_atted
 
 
+# ------------------------
+# ---- Self Attention ----
+# ------------------------
+
+class SA(nn.Module):
+    def __init__(self, args):
+        super(SA, self).__init__()
+
+        self.mhatt = MHAtt(args)
+        self.ffn = FFN(args)
+
+        self.dropout1 = nn.Dropout(args.dropout_r)
+        self.norm1 = LayerNorm(args.hidden_size)
+
+        self.dropout2 = nn.Dropout(args.dropout_r)
+        self.norm2 = LayerNorm(args.hidden_size)
+
+    def forward(self, y, y_mask):
+        y = self.norm1(y + self.dropout1(
+            self.mhatt(y, y, y, y_mask)
+        ))
+
+        y = self.norm2(y + self.dropout2(
+            self.ffn(y)
+        ))
+
+        return y
+
+
+# -------------------------------
+# ---- Self Guided Attention ----
+# -------------------------------
+
+class SGA(nn.Module):
+    def __init__(self, args):
+        super(SGA, self).__init__()
+
+        self.mhatt1 = MHAtt(args)
+        self.mhatt2 = MHAtt(args)
+        self.ffn = FFN(args)
+
+        self.dropout1 = nn.Dropout(args.dropout_r)
+        self.norm1 = LayerNorm(args.hidden_size)
+
+        self.dropout2 = nn.Dropout(args.dropout_r)
+        self.norm2 = LayerNorm(args.hidden_size)
+
+        self.dropout3 = nn.Dropout(args.dropout_r)
+        self.norm3 = LayerNorm(args.hidden_size)
+
+    def forward(self, x, y, x_mask, y_mask):
+        x = self.norm1(x + self.dropout1(
+            self.mhatt1(v=x, k=x, q=x, mask=x_mask)
+        ))
+
+        x = self.norm2(x + self.dropout2(
+            self.mhatt2(v=y, k=y, q=x, mask=y_mask)
+        ))
+
+        x = self.norm3(x + self.dropout3(
+            self.ffn(x)
+        ))
+
+        return x
+
 # ------------------------------
 # ---- Multi-Head Attention ----
 # ------------------------------
@@ -65,11 +156,21 @@ class MHAtt(nn.Module):
         super(MHAtt, self).__init__()
         self.args = args
 
+        self.linear_v = nn.Linear(args.hidden_size, args.hidden_size)
         self.linear_k = nn.Linear(args.hidden_size, args.hidden_size)
         self.linear_q = nn.Linear(args.hidden_size, args.hidden_size)
+        self.linear_merge = nn.Linear(args.hidden_size, args.hidden_size)
 
-    def forward(self, k, q):
+        self.dropout = nn.Dropout(args.dropout_r)
+
+    def forward(self, v, k, q, mask):
         n_batches = q.size(0)
+        v = self.linear_v(v).view(
+            n_batches,
+            -1,
+            self.args.multi_head,
+            int(self.args.hidden_size / self.args.multi_head)
+        ).transpose(1, 2)
 
         k = self.linear_k(k).view(
             n_batches,
@@ -85,13 +186,31 @@ class MHAtt(nn.Module):
             int(self.args.hidden_size / self.args.multi_head)
         ).transpose(1, 2)
 
-        d_k = q.size(-1)
+        atted = self.att(v, k, q, mask)
+
+        atted = atted.transpose(1, 2).contiguous().view(
+            n_batches,
+            -1,
+            self.args.hidden_size
+        )
+        atted = self.linear_merge(atted)
+
+        return atted
+
+    def att(self, value, key, query, mask):
+        d_k = query.size(-1)
 
         scores = torch.matmul(
-            q, k.transpose(-2, -1)
+            query, key.transpose(-2, -1)
         ) / math.sqrt(d_k)
 
-        return scores
+        if mask is not None:
+            scores = scores.masked_fill(mask, -1e9)
+
+        att_map = F.softmax(scores, dim=-1)
+        att_map = self.dropout(att_map)
+
+        return torch.matmul(att_map, value)
 
 
 # ---------------------------
@@ -113,103 +232,9 @@ class FFN(nn.Module):
     def forward(self, x):
         return self.mlp(x)
 
-
-
-# -------------------------------
-# ---- Self Guided Attention ----
-# -------------------------------
-
-
-
-class SAC(nn.Module):
-    def __init__(self, args):
-        super(SAC, self).__init__()
-
-        self.args = args
-        self.mhatt = MHAtt(args)
-        self.dropout = nn.Dropout(args.dropout_r)
-        self.linear_vx = nn.Linear(args.hidden_size, args.hidden_size)
-        self.linear_mergex = nn.Linear(args.hidden_size, args.hidden_size)
-
-        self.linear_vy = nn.Linear(args.hidden_size, args.hidden_size)
-        self.linear_mergey = nn.Linear(args.hidden_size, args.hidden_size)
-
-    def forward(self, x, y, x_mask=None, y_mask=None):
-        n_batches = x.size(0)
-
-        scores = self.mhatt(x, y)
-
-
-        #MASKING
-        scoresx = scores
-        if x_mask is not None:
-            scoresx = scores.masked_fill(x_mask, -1e9)
-        att_mapx = F.softmax(scoresx, dim=-1)
-        att_mapx = self.dropout(att_mapx)
-
-        #ATTENDING
-        attedx = torch.matmul(att_mapx, self.linear_vx(x).view(
-            n_batches,
-            -1,
-            self.args.multi_head,
-            int(self.args.hidden_size / self.args.multi_head)
-        ).transpose(1, 2))
-        attedx = attedx.transpose(1, 2).contiguous().view(
-            n_batches,
-            -1,
-            self.args.hidden_size
-        )
-        mergex = self.linear_mergex(attedx)
-
-        # QV^T = (VQ^T)T
-        scoresy = scores.transpose(-2, -1)
-        if y_mask is not None:
-            scoresy = scoresy.masked_fill(y_mask, -1e9)
-        att_mapy = F.softmax(scoresy, dim=-1)
-        att_mapy = self.dropout(att_mapy)
-        attedy = torch.matmul(att_mapy, self.linear_vy(y).view(
-            n_batches,
-            -1,
-            self.args.multi_head,
-            int(self.args.hidden_size / self.args.multi_head)
-        ).transpose(1, 2))
-        attedy = attedy.transpose(1, 2).contiguous().view(
-            n_batches,
-            -1,
-            self.args.hidden_size
-        )
-        mergey = self.linear_mergey(attedy)
-        # residual
-        y = self.dropout(mergex) + y
-        x = self.dropout(mergey) + x
-
-        return x, y
-
-class Att_solo(nn.Module):
-    def __init__(self, args, FLAT_GLIMPSES):
-        super(Att_solo, self).__init__()
-        self.args = args
-        self.linear = nn.Linear(args.ff_size, FLAT_GLIMPSES)
-        self.FLAT_GLIMPSES = FLAT_GLIMPSES
-
-    def forward(self, x, q, x_mask=None):
-        att = self.linear(q)
-        if x_mask is not None:
-            att = att.masked_fill(
-                x_mask.squeeze(1).squeeze(1).unsqueeze(2),
-                -1e9
-            )
-        att = F.softmax(att, dim=1)
-
-        att_list = []
-        for i in range(self.FLAT_GLIMPSES):
-            att_list.append(
-                torch.sum(att[:, :, i: i + 1] * x, dim=1)
-            )
-
-        return torch.stack(att_list).transpose_(0, 1)
-
-
+# ---------------------------
+# ---- FF + norm  -----------
+# ---------------------------
 class FFAndNorm(nn.Module):
     def __init__(self, args):
         super(FFAndNorm, self).__init__()
@@ -257,6 +282,7 @@ class Model_bi(nn.Module):
 
         self.args = args
 
+        # LSTM
         self.embedding = nn.Embedding(
             num_embeddings=vocab_size,
             embedding_dim=args.word_embed_size
@@ -279,18 +305,18 @@ class Model_bi(nn.Module):
         #     batch_first=True
         # )
 
-
+        # Feature size to hid size
         self.adapter = nn.Linear(args.audio_feat_size, args.hidden_size)
 
-        self.dec_list = nn.ModuleList([Block(args) for _ in range(args.layer)])
+        # Encoder blocks
+        self.enc_list = nn.ModuleList([Block(args) for _ in range(args.layer)])
 
-        #flattening
+        # Flattenting features before proj
         self.attflat_img = AttFlat(args)
         self.attflat_lang = AttFlat(args)
 
         # Classification layers
         self.proj_norm = LayerNorm(2 * args.hidden_size)
-
         if self.args.task == "sentiment":
             if self.args.task_binary:
                 self.proj = nn.Linear(2 * args.hidden_size, 2)
@@ -310,13 +336,12 @@ class Model_bi(nn.Module):
 
         y = self.adapter(y)
 
-        for i, dec in enumerate(self.dec_list):
+        for i, dec in enumerate(self.enc_list):
             x_m, x_y = None, None
             if i == 0:
                 x_m, x_y = x_mask, y_mask
             x, y = dec(x, x_m, y, x_y)
 
-        # Flatten to vector #enlever ????
         x = self.attflat_lang(
             x,
             x_mask
@@ -333,6 +358,3 @@ class Model_bi(nn.Module):
         ans = self.proj(proj_feat)
 
         return ans
-
-
-
